ecc7e0687cf491c2cd84e8858922ad1a655a2cff
herihendrayana <58313059+herihendrayana@users.noreply.github.com>
2020-08-26 20:13:45 +0700

    Initialize bug/fix-csp-safari-hopefully

---
+++ common/v2/services/index.ts
@@ -0,0 +1,5 @@
+export * from './Analytics';
+export * from './API';
+export * from './Cache';
+export * from './ShapeShift';
+export * from './Storage';
---
+++ shared/types/network.d.ts
@@ -0,0 +1,112 @@
+import { TAB } from 'components/Header/components/constants';
+
+type StaticNetworkIds =
+  | 'ETH'
+  | 'Ropsten'
+  | 'Kovan'
+  | 'Rinkeby'
+  | 'Goerli'
+  | 'ETC'
+  | 'RSK'
+  | 'AKA'
+  | 'AQUA'
+  | 'ARTIS_SIGMA1'
+  | 'ARTIS_TAU1'
+  | 'ATH'
+  | 'CLO'
+  | 'DEXON'
+  | 'EGEM'
+  | 'ELLA'
+  | 'EOSC'
+  | 'ESN'
+  | 'ETI'
+  | 'ETHO'
+  | 'ETSC'
+  | 'EXP'
+  | 'Gangnam'
+  | 'GO'
+  | 'GO_TESTNET'
+  | 'METADIUM'
+  | 'MIX'
+  | 'MUSIC'
+  | 'PIRL'
+  | 'POA'
+  | 'REOSC'
+  | 'RSK_TESTNET'
+  | 'SOLIDUM'
+  | 'THUNDERCORE'
+  | 'TOMO'
+  | 'UBQ'
+  | 'WEB'
+  | 'AUX'
+  | 'ASK';
+
+export interface BlockExplorerConfig {
+  name: string;
+  origin: string;
+  txUrl(txHash: string): string;
+  addressUrl(address: string): string;
+  blockUrl(blockNum: string | number): string;
+}
+
+interface Token {
+  address: string;
+  symbol: string;
+  decimal: number;
+  error?: string | null;
+}
+
+interface NetworkContract {
+  name: StaticNetworkIds;
+  address?: string;
+  abi: string;
+}
+
+interface DPathFormats {
+  trezor?: DPath;
+  safeTmini?: DPath;
+  ledgerNanoS?: DPath;
+  mnemonicPhrase: DPath;
+}
+
+export interface GasPriceSetting {
+  min: number;
+  max: number;
+  initial: number;
+}
+
+interface StaticNetworkConfig {
+  isCustom: false; // used for type guards
+  id: StaticNetworkIds;
+  name: string;
+  unit: string;
+  color?: string;
+  blockExplorer: BlockExplorerConfig;
+  tokenExplorer?: {
+    name: string;
+    address(address: string): string;
+  };
+  chainId: number;
+  tokens: Token[];
+  contracts: NetworkContract[] | null;
+  dPathFormats: DPathFormats;
+  isTestnet?: boolean;
+  gasPriceSettings: GasPriceSetting;
+  shouldEstimateGasPrice?: boolean;
+  unsupportedTabs?: TAB[];
+  hideEquivalentValues?: boolean;
+}
+
+interface CustomNetworkConfig {
+  isCustom: true; // used for type guards
+  isTestnet?: boolean;
+  id: string;
+  name: string;
+  unit: string;
+  chainId: number;
+  dPathFormats: DPathFormats | null;
+  unsupportedTabs?: TAB[];
+  hideEquivalentValues?: boolean;
+}
+
+type NetworkConfig = CustomNetworkConfig | StaticNetworkConfig;
